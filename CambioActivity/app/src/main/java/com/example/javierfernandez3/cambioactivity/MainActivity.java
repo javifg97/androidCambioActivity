9b8389ee4ae4947a96f8192e29463b590c406413
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2017-11-15 19:13:16 +0100

    Primer commit

---
+++ CambioActivity/app/src/main/java/com/example/javierfernandez3/cambioactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.javierfernandez3.cambioactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
